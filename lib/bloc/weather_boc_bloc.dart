75a0684ed83bd9039adeefae2daf39bc30718529
akhilSNediyathu <akhilsnediyathu@gmail.com>
2024-04-21 20:09:03 +0530

    test1

--- lib/bloc/weather_boc_bloc.dart
+++ lib/bloc/weather_boc_bloc.dart
@@ -1,17 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
+import 'package:weather_app/data/api_keys.dart';
 
 part 'weather_boc_event.dart';
 part 'weather_boc_state.dart';
 
 class WeatherBocBloc extends Bloc<WeatherBocEvent, WeatherBocState> {
   WeatherBocBloc() : super(WeatherBocInitial()) {
-    on<FetchWeather>((event, emit) {
+    on<FetchWeather>((event, emit) async {
        emit(WeatherBocLoading());
       try {
-        WeatherFactory wf = WeatherFactory(_apikey,language: Language.ENGLISH);
-        emit(WeatherBocSuccess());
+        WeatherFactory wf = WeatherFactory(apikey,language: Language.ENGLISH);
+     Position position =   await Geolocator.getCurrentPosition();
+        Weather weather = await wf.currentWeatherByLocation(
+          position.latitude,
+          position.longitude
+          );
+        emit(WeatherBocSuccess(weather: weather));
         
       } catch (e) {
         emit(WeatherBocFailure());
